383932db6e51921817ed8ffab8e39851fc04a4b4
SekiroK5 <arin01246@gmail.com>
2026-04-24 11:11:51 -0600

    fix: corrección de error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/interfaces/auth.module';
 import { TaskModule } from './modules/task/interfaces/task.module';
 import { UserModule } from './modules/user/interfaces/user.module';
@@ -6,7 +7,13 @@ import { PrismaService } from './common/services/prisma.service';
 import { CommonModule } from './common/common';
 
 @Module({
-  imports: [AuthModule, TaskModule, UserModule, CommonModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule, 
+    TaskModule, 
+    UserModule, 
+    CommonModule
+  ],
   providers: [PrismaService],
 })
 export class AppModule { }
